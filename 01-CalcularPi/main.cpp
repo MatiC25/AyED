86d68d57d54a5035aba1646bfb01fb0d5e1cc1ab
MatiC25 <131481746+MatiC25@users.noreply.github.com>
2023-05-24 15:21:23 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -1,1 +1,23 @@
-
+//Calcular pi
+#include <iostream>
+#include <iomanip>
+using namespace std; 
+int main()
+ {
+    int i , n;
+    
+    double pi , denominador;
+    
+    pi=0; n=1; i=1;   
+    
+    while (i<=1000000){
+        denominador=(2*i-1);// 2*i-1 formula de numeros impares 
+        pi=pi+n*(1/denominador); //Sumatoria del anterior 
+        n = n*(-1); //Alternación de signo al realizar cada suma 
+        i++; 
+    }
+     cout<<"El numero de pi aproximado es de: "<< setprecision(7)<< pi*4; // Setprecision sirve para asignar un numero de digitos especificos
+     // Pi por 4 ya que la serie de Leibniz da como resultado pi/4
+    
+    return 0;
+    }
